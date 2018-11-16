425d503f346641f9802cbf43e4e10f7e3516e583
pio1755 <44492933+pio1755@users.noreply.github.com>
2018-11-16 01:07:40 +0100

    Update na_gita2.cpp
    z prawdziwym bledem

--- na_gita2.cpp
+++ na_gita2.cpp
@@ -5,7 +5,7 @@ int main()
     int podana1,podana2;
     printf("podaj 2 liczby ktore program ma zsuowac\n");
     scanf("%d",&podana1);
-    scanf("%d",&podana2);
+    scanf("%d",&podana2)
 
     printf("suma liczb %d oraz %d wynosi %d",podana1,podana2,podana1+podana2);
 
