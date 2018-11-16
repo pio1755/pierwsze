8e6cffd314872a0606821fedd83e68d4c2380677
pio1755 <pio1755@gmail.com>
2018-11-16 01:07:01 +0100

    z bledem

---
+++ na_gita2.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int podana1,podana2;
+    printf("podaj 2 liczby ktore program ma zsuowac\n");
+    scanf("%d",&podana1);
+    scanf("%d",&podana2);
+
+    printf("suma liczb %d oraz %d wynosi %d",podana1,podana2,podana1+podana2);
+
+
+
+
+    return 0;
+}
